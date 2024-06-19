80936baf74098d8fe0b85ddb4fc0f7b3a5c414d2
nome-git <168424276+AntonioNorfo@users.noreply.github.com>
2024-06-19 16:55:08 +0200

    Esercizio3js

--- Esercizio3JavaScript/assets/js/script.js
+++ Esercizio3JavaScript/assets/js/script.js
@@ -151,13 +151,17 @@ for (let i = 0; i < starWarsCharacters.length; i++) {
     case "blue":
       eyeColor.blue.push(starWarsCharacters[i]);
       break;
-    case yellow:
+    case "yellow":
+      eyeColor.yellow.push(starWarsCharacters[i]);
       break;
-    case brown:
+    case "brown":
+      eyeColor.brown.push(starWarsCharacters[i]);
       break;
-    case red:
+    case "red":
+      eyeColor.red.push(starWarsCharacters[i]);
       break;
-    case blueGray:
+    case "blueGray":
+      eyeColor.blueGray.push(starWarsCharacters[i]);
       break;
   }
 }
@@ -171,9 +175,8 @@ let i = 0;
 while (i < starWarsCharacters.length) {
   crewMass += starWarsCharacters[i].mass;
   i++;
-
-  console.log("La massa totale e' " + crewMass);
 }
+console.log("La massa totale e' " + crewMass);
 
 /* ESERCIZIO 7
     Crea uno if/else statement per rivelare la tipologia di carico, utilizzando la massa totale, di un'ipotetica astronave contenente i personaggi dell'array "starWarsCharacters".
@@ -185,11 +188,31 @@ while (i < starWarsCharacters.length) {
     Se la massa è superiore a 1000 stampa in console: "DANGER! OVERLOAD ALERT: escape from ship now!"
   
     Una volta fatto, modifica la massa di qualche elemento dell'equipaggio e vedi se riesci ad ottenere un messaggio diverso.
-  */
+*/
+
+if (crewMass < 500) {
+  console.log("Ship is under loaded");
+} else if (crewMass > 500 && crewMass < 700) {
+  console.log("Ship is half loaded");
+} else if (crewMass > 700 && crewMass < 900) {
+  console.log("Warning: Load is over 700");
+} else if (crewMass > 900 && crewMass < 1000) {
+  console.log("Critical Load: Over 900");
+} else {
+  crewMass > 1000;
+  console.log("DANGER! OVERLOAD ALERT: escape from ship now!");
+}
 
 /* ESERCIZIO 8
     Usa un for loop per cambiare il valore della proprietà "gender" di alcuni personaggi dal valore "n/a" a "robot" (Tip: puoi effettuare la riassegnazione del valore corrispondente o creare un nuovo array)
   */
+let = 0;
+for (let i = 0; i < starWarsCharacters.length; i++) {
+  if (starWarsCharacters[i].gender === "n/a") {
+    starWarsCharacters[i].gender = "robot";
+  }
+}
+console.log(starWarsCharacters);
 
 /* --EXTRA-- ESERCIZIO 9
     Utilizzando gli elementi presenti nell'array "femaleCharacters" rimuovi dall'array "charactersNames" le stringhe corrispondenti a personaggi con lo stesso nome.
