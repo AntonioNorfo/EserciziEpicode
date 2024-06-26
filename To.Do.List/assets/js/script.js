d296a485e4ff17cf176bced16a8e4d2a62a3cac1
nome-git <168424276+AntonioNorfo@users.noreply.github.com>
2024-06-26 20:38:00 +0200

    ToDoList

--- To.Do.List/assets/js/script.js
+++ To.Do.List/assets/js/script.js
@@ -1,48 +1,30 @@
-const myList = document.getElementById("myList");
 const sendForm = document.getElementById("sendForm");
 const btnButton = document.getElementById("btnButton");
 const listP = document.getElementById("listP");
 const newLabel = [];
+btnButton.addEventListener("click", () => {
+  //  e.preventDefault();
+  itemsNewLabel();
+});
+function itemsNewLabel() {
+  newLabel.push(sendForm.value);
+  console.log(newLabel);
+  printListP();
+  sendForm.value = "";
+}
 
-window.addEventListener("load", init);
-
-function init() {
-  btnButton.addEventListener("click", function (e) {
-    e.preventDefault();
-    if (!checkInput()) return;
-    itemsNewLabel();
-    printListP();
-    myList.reset();
-  });
-  function checkInput() {
-    if (sendForm.value === "") {
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  function itemsNewLabel() {
-    newLabel.push(sendForm.value);
-    console.log(newLabel);
-  }
-
-  function printListP() {
-    listP.innerHTML = " ";
-    for (let i = 0; i < newLabel.length; i++) {
-      let componentNewLabel = document.createElement("p");
-      componentNewLabel.innerText = newLabel[i];
-      let btnDelete = document.createElement("button");
-      btnDelete.setAttribute("type", "button");
-      btnDelete.setAttribute("onclick", `deleteItem(${i});`);
-      btnDelete.innerHTML = '<i class="fas fa-trash"></i>';
-      componentNewLabel.appendChild(btnDelete);
-      listP.appendChild(componentNewLabel);
-    }
-  }
-
-  function deleteItem(index) {
-    newLabel.splice(index, 1);
-    printListP();
+function printListP() {
+  listP.innerHTML = " ";
+  for (let i = 0; i < newLabel.length; i++) {
+    let componentNewLabel = document.createElement("p");
+    componentNewLabel.innerText = newLabel[i];
+    let btnDelete = document.createElement("button");
+    btnDelete.addEventListener("click", () => {
+      newLabel.splice(i, 1);
+      printListP();
+    });
+    btnDelete.innerHTML = '<i class="fas fa-trash"></i>';
+    componentNewLabel.appendChild(btnDelete);
+    listP.appendChild(componentNewLabel);
   }
 }
